a938160f25667d46631fedbd34fc5792aaace73b
Tejas-777 <chaudharitejas769@gmail.com>
2024-05-15 21:32:33 +0530

    pattern

---
+++ Patterns/Pattern_20.java
@@ -0,0 +1,30 @@
+/*
+
+ *        *
+ **      **
+ ***    ***
+ ****  ****
+ **********
+ ****  ****
+ ***    ***
+ **      **
+ *        *
+
+ */
+
+import java.util.*;
+public class Pattern_20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows : ");
+        int n = sc.nextInt();
+        printpattern(n);
+
+    }
+
+    public static void printpattern(int n){
+        for(int i=1; i<=(2*n)-1; i++){
+
+        }
+    }
+}
